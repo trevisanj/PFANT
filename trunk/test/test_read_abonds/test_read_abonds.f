9aa777860b4f205460da5712388f3dba0cdaeb2a
juliotrevisan@gmail.com <juliotrevisan@gmail.com@1f3cacde-0413-1a4d-b44a-7d8e6d1e3204>
2015-03-12 23:42:43 +0000

    test_read_abonds

---
+++ trunk/test/test_read_abonds/test_read_abonds.f
@@ -0,0 +1,17 @@
+
+C     TEST THE READ_MAIN ROUTINE
+      PROGRAM TEST_READ_ABONDS
+      USE COMMONS
+
+      CALL READ_ABONDS('abonds.dat')
+
+      WRITE (*, *) 'abonds_NABOND = ', abonds_NABOND
+      WRITE (*, *) ''
+
+      WRITE (*, *) 'EL Abundance'
+      WRITE (*, *) '============'
+      DO I = 1, abonds_NABOND
+        WRITE(*, '(1X, A2, F10.3)') abonds_ELE(I), abonds_ABOL(I)
+      END DO
+
+      END
