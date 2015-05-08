bc05a0b3ff5173e9cb124d195a505b84b3952ec6
trevisanj <juliotrevisan@gmail.com>
2015-05-08 19:30:12 -0300

    Finished explaining git repository

--- main/repository.cpp
+++ main/repository.cpp
@@ -1,7 +1,8 @@
 /**
 @page repository Working with the GitHub repository
 
-@section Fetching the branches
+Fetching the branches
+=====================
 
 The repository has two branches:
 @li @c master All Python and Fortran source code
@@ -11,7 +12,7 @@ When working on your computer locally, in order to have Doxygen parsing files fr
 @c master branch while generating output directly into
 the @c gh-pages branch, it is necessary to work with the *two branches of the repository
 as parallel local directories*, fetched with the <code>git clone</code>
-<b>--single-branch</code> option.
+<b>--single-branch</b></code> option.
 
 @note Cloning only one branch is not the default behaviuor of git. The default is to
 clone all the branches and then switch between branches with the <code>git checkout</code>
@@ -21,20 +22,28 @@ won't be able to use Doxygen as expected.
 Here is a set of commands to fetch the two branches separately:
 
 @code
+# It is good to be inside a directory called "github" on your computer
 $ pwd
 /home/j/Documents/projects/pfant/github
-# Good to be inside a directory called "github" on your computer
-$ git clone https://github.com/trevisanj/pfant --branch master --single-branch master
-# This will clone the "master" branch into a directory called "master"
-$ git clone https://github.com/trevisanj/pfant --branch gh-pages --single-branch gh-pages
-# This will clone the "gh-pages" branch into a directory called "gh-pages"
 
-@section Communicating with GitHub <i>via</i> SSH
+# This will clone the "master" branch into a directory called "master"
+$ git clone https://github.com/trevisanj/pfant --branch master --single-branch master
+
+# This will clone the "gh-pages" branch into a directory called "gh-pages"
+$ git clone https://github.com/trevisanj/pfant --branch gh-pages --single-branch gh-pages
+
+$ ls
+gh-pages  master
+@endcode
+
+Communicating with GitHub <i>via</i> SSH
+========================================
 
 Using the SSH protocol instead of HTTPS will at least keep git from asking for your
 username and password all the time. 
 
-Roughly the steps are:
+The steps involved are roughly summarized below. For full details, please follow the
+tutorials referenced below.
 <ol>
 
   <li>Generate a SSH key. I followed the tutorial at 
@@ -49,23 +58,24 @@ Roughly the steps are:
   </li>
   
   <li>Copy-paste the key to a box at a settings page at GitHub. 
-  You can test if it works using 
+  After you do this, you can test if it works:
   @code
   $ ssh -T git@github.com
   @endcode
   </li>
   
-  <li>Change the remote URL that git uses.
+  <li>Change the remote URL that git uses. Tutorial at
   https://help.github.com/articles/changing-a-remote-s-url/
   @code
   $ git remote set-url origin git@github.com:trevisanj/pfant.git
   @endcode
   </li>
-<ol>
+</ol>
 
-@note I might have forgotten some detail. The tutorials explain better, anyway.
+References
+==========
 
-https://help.github.com/articles/generating-ssh-keys/, 
+https://help.github.com/articles/generating-ssh-keys/
 
 https://help.github.com/articles/changing-a-remote-s-url/
 
