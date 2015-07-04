0a4ad48653ba3e9830b4e1889321e2dedeaea97c
trevisanj <juliotrevisan@gmail.com>
2015-07-03 23:23:24 -0300

    Update and rename main.f90 to nulbad.f90

---
+++ fortran/nulbad/nulbad.f90
@@ -0,0 +1,37 @@
+! New plan for nulbad
+! 1) Get rid of internal setup
+! 2) Implement command-line + main.dat setup
+! 3) Write nulbad.f90 file
+
+!> programme NULBT=NULBAD_SQ
+!>
+!>  lecture et convolution sortie fant93
+!>
+!>  Programme de lecture des flux en binaire, sortant de
+!>      Fantomol93 , calculs de 100A en 100A (ou inferieur a 100A)
+!>
+!> Paula, julho de 2003:
+!> - modifiquei para nao normalizar o espectro
+!> - a opcao config_convol=F nao estava imprimindo o lambda, corrigi
+!> P.Coelho, dez 2003
+!> - se config_norm = .TRUE. (saida do pfant ja eh normalizada), nao
+!>  altera o valor do fluxo
+
+
+program nulbad
+  use config_nulbad
+  use logging
+  use welcome
+  use nulbad_calc
+  implicit none
+
+  ! startup
+  write(*,*) pfant_version('NULBAD')
+  write(*,*) ''
+  call config_init()
+  call print_welcome(6)
+
+  ! calculation
+  call nulbad_calc_()
+end
+
