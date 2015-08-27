48d4e3a4a0b9f21e2d4fc6edb6d72b6fd8e0095e
trevisanj <juliotrevisan@gmail.com>
2015-08-26 23:41:30 -0300

    deploy

--- push_doxy-fortran.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script pushed the doxy-fortran directory into the gh-pages branch at github
-#
-# Reference: https://gist.github.com/cobyism/4730490
-
-git subtree push --prefix doxy-fortran origin gh-pages
