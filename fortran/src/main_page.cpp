5420b14931b9087a59b4b35b8acd9252675be33c
trevisanj <juliotrevisan@gmail.com>
2015-10-21 17:45:12 -0200

    Update main_page.cpp

--- fortran/src/main_page.cpp
+++ fortran/src/main_page.cpp
@@ -15,11 +15,11 @@ PFANT is a suite consisting of four executables:
 
 @verbatim
  |
-t|     FANTÔME   - French for "ghost", "spectrum"
-i|     FANTOMOL  - molecular calculus added
-m|    PFANT      - "P" as in Perrin, Marie-Noel
-e|
+ |     FANTÔME   - French for "ghost", "spectrum"
+ |     FANTOMOL  - molecular calculus added
+ |    PFANT      - "P" as in Perrin, Marie-Noel
  |
+t|
  V
 @endverbatim
 
@@ -29,40 +29,5 @@ Quick links
 @ref overview
 
 @ref coding_guide
-
-*/
-
-/*
-I THink I won't need these groups no longer with Doxygen 1.8.9
-
-
-@defgroup gr_config System
-@brief System configuration and startup
-THis group contains modules and routines dedicated to setting up the environment state
-for the numerical calculations.
-
-@defgroup gr_math Maths
-
-@defgroup gr_io Input/Output
-
-@brief I/O and logging routines.
-This group contains
-@li routines to read input files
-@li logging routines
-
-@defgroup gr_data DATA statements
-@brief Places where you can find @c DATA statements.
-This group gathers all modules and subroutines that contain @c DATA statements.
-
-@defgroup gr_filter Filtering routines
-@brief @c lzero - @c lfin -dependent filtering
-
-This group gathers all routines that filter arrays depending on @c lzero - @c lfin wavelength
-interval.
-
-Filtering is analogous to SQL
-@code
-SELECT ... WHERE lzero <= &lambda; <= lfin
-@endcode
 
 */
