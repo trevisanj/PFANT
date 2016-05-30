1e2f27fdb04359790255b20510cc9d884a958983
trevisanj <garapuvu1@gmail.com>
2016-05-30 11:28:15 -0300

    moved .f90 files

--- fortran/make-windows.bat
+++
@@ -1,9 +0,0 @@
-@rem To make sure no old executables remain, we explicitly delete them.
-del bin\*.*
-del obj-windows*.* /s
-
-make -f makefile-windows-innewmarcs
-make -f makefile-windows-hydro2
-make -f makefile-windows-pfant
-make -f makefile-windows-nulbad
-
