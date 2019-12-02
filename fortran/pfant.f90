0837b0928533979dfb62d95f736e7a26095a3ac0
Julio Trevisan <juliotrevisan@gmail.com>
2019-12-02 11:25:07 -0200

    investigating...

--- fortran/pfant.f90
+++ fortran/pfant.f90
@@ -1188,79 +1188,79 @@ end module
 !
 
 program pfant
-  use pfantlib
-  use synthesis
-  use pfant_x
-  implicit none
-  integer i
-  logical dissoc_exists
-  real*8 temp
+!@  use pfantlib
+!@  use synthesis
+!@  use pfant_x
+!@  implicit none
+!@  integer i
+!@  logical dissoc_exists
+!@  real*8 temp
 
   !=====
   ! Startup
   !=====
-  execonf_name = 'pfant'
-  call config_init()
+!@  execonf_name = 'pfant'
+!@  call config_init()
 
   !=====
   ! File reading
   !=====
-  call read_main(config_fn_main)
+!@  call read_main(config_fn_main)
 
   !---
   ! (intermission)
   ! After reading *main file*, initializes variables whose values may come
   ! either from *main file* or command-line option
   !---
-  call pfant_init_x()
+!@  call pfant_init_x()
 
   ! x_aint has to be divisible by x_pas
-  temp = x_aint/x_pas
-  if (abs(temp-nint(temp)) .gt. 1.e-10) then
-    call log_and_halt('pas='//real82str(x_pas, 5)//' (delta-lambda) must be a '//&
-     'sub-multiple of aint='//real82str(x_aint, 1)//' (they are set in '//&
-     'main configuration file, or command-line options "--aint" and "--pas").')
-  end if
+!@  temp = x_aint/x_pas
+!@  if (abs(temp-nint(temp)) .gt. 1.e-10) then
+!@    call log_and_halt('pas='//real82str(x_pas, 5)//' (delta-lambda) must be a '//&
+!@     'sub-multiple of aint='//real82str(x_aint, 1)//' (they are set in '//&
+!@     'main configuration file, or command-line options "--aint" and "--pas").')
+!@  end if
 
 
   ! continues file reading
-  call read_abonds(config_fn_abonds)
-  inquire(file=config_fn_dissoc, exist=dissoc_exists)
-  if (dissoc_exists) then
-    call read_dissoc(config_fn_dissoc)
-  else
-    call log_warning('File "'//trim(config_fn_dissoc)//'" not found: will take '//&
-     'internally stored template and replace abundances with those in "'//trim(config_fn_abonds)//'" -12')
-    call auto_dissoc()
-  end if
+!@  call read_abonds(config_fn_abonds)
+!@  inquire(file=config_fn_dissoc, exist=dissoc_exists)
+!@  if (dissoc_exists) then
+!@    call read_dissoc(config_fn_dissoc)
+!@  else
+!@    call log_warning('File "'//trim(config_fn_dissoc)//'" not found: will take '//&
+!@     'internally stored template and replace abundances with those in "'//trim(config_fn_abonds)//'" -12')
+!@    call auto_dissoc()
+!@  end if
 
-  call read_partit(config_fn_partit)  ! LECTURE DES FCTS DE PARTITION
-  call read_absoru2(config_fn_absoru2)  ! LECTURE DES DONNEES ABSORPTION CONTINUE
-  call read_modele(config_fn_modeles)  ! LECTURE DU MODELE
-  if (config_opa) call read_opa(config_fn_opa)
-  if (.not. config_no_atoms) then
-    call read_atoms(config_fn_atoms)
-    if (config_zinf .ne. -1) then
-      do i = 1, atoms_nblend
-        atoms_zinf(i) = config_zinf
-      end do
-    end if
-  end if
-  if (.not. config_no_h) then
-    call read_hmap(config_fn_hmap)
-    call read_filetoh(x_llzero, x_llfin)
-  end if
-  if (.not. config_no_molecules) call read_molecules(config_fn_molecules)
+!@  call read_partit(config_fn_partit)  ! LECTURE DES FCTS DE PARTITION
+!@  call read_absoru2(config_fn_absoru2)  ! LECTURE DES DONNEES ABSORPTION CONTINUE
+!@  call read_modele(config_fn_modeles)  ! LECTURE DU MODELE
+!@  if (config_opa) call read_opa(config_fn_opa)
+!@  if (.not. config_no_atoms) then
+!@    call read_atoms(config_fn_atoms)
+!@    if (config_zinf .ne. -1) then
+!@      do i = 1, atoms_nblend
+!@        atoms_zinf(i) = config_zinf
+!@      end do
+!@    end if
+!@  end if
+!@  if (.not. config_no_h) then
+!@    call read_hmap(config_fn_hmap)
+!@    call read_filetoh(x_llzero, x_llfin)
+!@  end if
+!@  if (.not. config_no_molecules) call read_molecules(config_fn_molecules)
 
-  if (abs(modele%asalog-main_afstar) > 0.01) then
-    call log_and_halt('asalog from model ('//real82str(modele%asalog, 2)//&
-     ') does not match afstar in main configuration file ('//real82str(main_afstar, 2)//')')
-  end if
+!@  if (abs(modele%asalog-main_afstar) > 0.01) then
+!@    call log_and_halt('asalog from model ('//real82str(modele%asalog, 2)//&
+!@     ') does not match afstar in main configuration file ('//real82str(main_afstar, 2)//')')
+!@  end if
 
 
   !=====
   ! Spectral synthesis
   !=====
   ! Does the calculus
-  call synthesis_()
+!@  call synthesis_()
 end program pfant
